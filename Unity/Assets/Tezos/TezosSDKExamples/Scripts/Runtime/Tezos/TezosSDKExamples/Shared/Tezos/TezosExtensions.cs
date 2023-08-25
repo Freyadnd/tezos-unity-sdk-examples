7e3ca0537b54773a238a7eac59d73f7ea2e2f48a
Freya <tz428@cornell.edu>
2023-08-25 09:00:00 -0700

    fix: improve network error handling and logging for Tezos API calls

--- Unity/Assets/Tezos/TezosSDKExamples/Scripts/Runtime/Tezos/TezosSDKExamples/Shared/Tezos/TezosExtensions.cs
+++ Unity/Assets/Tezos/TezosSDKExamples/Scripts/Runtime/Tezos/TezosSDKExamples/Shared/Tezos/TezosExtensions.cs
@@ -16,6 +16,15 @@ namespace TezosSDKExamples.Shared.Tezos
     /// TzKT indexer API (https://api.tzkt.io). Always await them — never fire-and-forget
     /// on the main thread unless you are certain the result is not needed immediately.
     ///
+    /// ERROR HANDLING OVERVIEW:
+    ///   All HTTP calls route through SendWithRetryAsync(), which retries on transient
+    ///   failures (connection errors, HTTP 429 Too Many Requests, HTTP 5xx server errors)
+    ///   up to MaxRetries times with a fixed RetryDelaySeconds delay between attempts.
+    ///   Non-retryable failures (HTTP 4xx, malformed JSON) fail immediately and return
+    ///   safe default values (false for ownership checks, empty lists for inventory).
+    ///   All failures are logged with a consistent [TezosExtensions] prefix so they are
+    ///   easy to filter in the Unity Console.
+    ///
     /// PAGINATION OVERVIEW:
     ///   TzKT paginates results using two query parameters:
     ///     limit  — how many records to return in one response (max 10000, practical max ~100)
@@ -65,6 +74,15 @@ namespace TezosSDKExamples.Shared.Tezos
         /// </summary>
         public const int MaxPageSize = 50;
 
+        // Maximum number of attempts for a single HTTP request before giving up.
+        // The first attempt counts, so MaxRetries = 3 means one initial try plus two retries.
+        private const int MaxRetries = 3;
+
+        // Seconds to wait between retry attempts.
+        // A fixed delay is sufficient for a game SDK. In a production service you would
+        // use exponential backoff: delay = RetryDelaySeconds * Math.Pow(2, attempt - 1).
+        private const float RetryDelaySeconds = 1.5f;
+
 
         // ---------------------------------------------------------------
         //  Authentication helpers
@@ -88,6 +106,9 @@ namespace TezosSDKExamples.Shared.Tezos
         /// Returns true if <paramref name="account"/> currently holds at least one
         /// unit of the token identified by <paramref name="contract"/> and <paramref name="tokenId"/>.
         ///
+        /// Returns false on network failure — treat a false result as "ownership unconfirmed"
+        /// rather than "definitively does not own" if you expect network issues.
+        ///
         /// Use this for token gating: checking whether the player owns a specific NFT
         /// before unlocking a feature, area, or character.
         /// </summary>
@@ -106,6 +127,8 @@ namespace TezosSDKExamples.Shared.Tezos
         /// Returns all tokens currently held by <paramref name="account"/>,
         /// fetching every page automatically until the full inventory is assembled.
         ///
+        /// Returns an empty list on unrecoverable network failure.
+        ///
         /// Suitable for wallets with small-to-moderate inventories. For large inventories
         /// (100+ tokens) or incremental UI loading, prefer
         /// <see cref="GetAllTokensForOwnerPaginated"/> or <see cref="GetTokensForOwnerPage"/>.
@@ -123,6 +146,8 @@ namespace TezosSDKExamples.Shared.Tezos
 
         /// <summary>
         /// Fetches one page of tokens held by <paramref name="account"/>.
+        ///
+        /// Returns an empty list on unrecoverable network failure.
         ///
         /// PAGINATION CONCEPT — Single page fetch:
         ///   Use this to implement "Load More" buttons or lazy-loading inventory grids.
@@ -157,6 +182,8 @@ namespace TezosSDKExamples.Shared.Tezos
         /// <summary>
         /// Fetches the complete token inventory of <paramref name="account"/> page by page,
         /// invoking <paramref name="onPageLoaded"/> after each page arrives.
+        ///
+        /// Returns the partial inventory assembled before any unrecoverable failure.
         ///
         /// PAGINATION CONCEPT — Progressive loading:
         ///   Rather than waiting for all tokens before updating the UI, this method lets
@@ -205,32 +232,31 @@ namespace TezosSDKExamples.Shared.Tezos
         /// <summary>
         /// Checks whether a specific token balance exists for an account.
         /// Requests only the id field — the smallest possible payload for a boolean check.
+        /// Returns false on any unrecoverable failure.
         /// </summary>
         private static async UniTask<bool> CheckTokenBalance(
             string account, string contract, int tokenId)
         {
+            string context = $"CheckTokenBalance({account}, {contract}#{tokenId})";
             string url = $"{TzKtBaseUrl}&account={account}" +
                          $"&token.contract={contract}" +
                          $"&token.tokenId={tokenId}" +
                          $"&select=id";
 
-            bool isOwner = false;
-            using (UnityWebRequest request = UnityWebRequest.Get(url))
+            HttpResult result = await SendWithRetryAsync(url, context);
+            if (!result.Success)
+                return false;
+
+            try
             {
-                await request.SendWebRequest();
-
-                if (request.result == UnityWebRequest.Result.ConnectionError ||
-                    request.result == UnityWebRequest.Result.DataProcessingError)
-                {
-                    Debug.LogError($"[TezosExtensions] CheckTokenBalance failed: {request.error}");
-                }
-                else
-                {
-                    isOwner = JsonHelper.FromJson<int>(request.downloadHandler.text).Length > 0;
-                }
+                return JsonHelper.FromJson<int>(result.Text).Length > 0;
             }
-
-            return isOwner;
+            catch (Exception ex)
+            {
+                Debug.LogError($"[TezosExtensions] {context} — JSON parse failed: {ex.Message}" +
+                               $"\n  Raw response: {Truncate(result.Text, 200)}");
+                return false;
+            }
         }
 
         /// <summary>
@@ -239,6 +265,7 @@ namespace TezosSDKExamples.Shared.Tezos
         /// This is the single HTTP-calling method for all inventory queries. Both
         /// <see cref="GetTokensForOwnerPage"/> and <see cref="FetchAllPages"/> delegate here,
         /// keeping network logic in one place.
+        /// Returns an empty list on any unrecoverable failure.
         /// </summary>
         /// <param name="account">Wallet address to query (tz1...).</param>
         /// <param name="offset">Number of records to skip (0 for the first page).</param>
@@ -246,7 +273,8 @@ namespace TezosSDKExamples.Shared.Tezos
         private static async UniTask<List<TokenBalance>> FetchTokensPage(
             string account, int offset, int limit)
         {
-            // limit and offset are the TzKT pagination parameters.
+            string context = $"FetchTokensPage({account}, offset={offset}, limit={limit})";
+
             // &sort.asc=id ensures stable ordering across pages — without a sort order,
             // TzKT may return overlapping or skipped records between requests.
             string url = $"{TzKtBaseUrl}" +
@@ -256,34 +284,31 @@ namespace TezosSDKExamples.Shared.Tezos
                          $"&offset={offset}" +
                          $"&limit={limit}";
 
-            List<TokenBalance> page = new List<TokenBalance>();
-            using (UnityWebRequest request = UnityWebRequest.Get(url))
+            HttpResult result = await SendWithRetryAsync(url, context);
+            if (!result.Success)
+                return new List<TokenBalance>();
+
+            try
             {
-                await request.SendWebRequest();
-
-                if (request.result == UnityWebRequest.Result.ConnectionError ||
-                    request.result == UnityWebRequest.Result.DataProcessingError)
-                {
-                    Debug.LogError($"[TezosExtensions] FetchTokensPage failed " +
-                                   $"(offset={offset}, limit={limit}): {request.error}");
-                }
-                else
-                {
-                    page = JsonHelper.FromJson<TokenBalance>(request.downloadHandler.text).ToList();
-                }
+                return JsonHelper.FromJson<TokenBalance>(result.Text).ToList();
             }
-
-            return page;
+            catch (Exception ex)
+            {
+                Debug.LogError($"[TezosExtensions] {context} — JSON parse failed: {ex.Message}" +
+                               $"\n  Raw response: {Truncate(result.Text, 200)}");
+                return new List<TokenBalance>();
+            }
         }
 
         /// <summary>
         /// Fetches all pages for an account sequentially, assembling the full inventory.
         /// Stops when a page returns fewer items than <paramref name="pageSize"/>,
-        /// which signals that the last page has been reached.
+        /// which signals that the last page has been reached. Also stops early if a page
+        /// fetch fails, returning the partial inventory collected so far.
         /// </summary>
         /// <param name="onPageLoaded">
-        ///   Optional callback invoked after each page. Receives the page list and its
-        ///   zero-based index. Null-safe — omit it for a silent full load.
+        ///   Optional callback invoked after each successful page. Receives the page list
+        ///   and its zero-based index. Null-safe — omit it for a silent full load.
         /// </param>
         private static async UniTask<List<TokenBalance>> FetchAllPages(
             string account,
@@ -298,26 +323,182 @@ namespace TezosSDKExamples.Shared.Tezos
                 int offset = pageIndex * pageSize;
                 List<TokenBalance> page = await FetchTokensPage(account, offset, pageSize);
 
-                if (page.Count > 0)
+                // An empty page on the first request means the wallet has no tokens,
+                // or the request failed. Either way, stop — there is nothing more to fetch.
+                // On subsequent pages, an empty result after a previous success means the
+                // fetch failed (FetchTokensPage returns [] on error). Stop and return what
+                // we have rather than looping forever.
+                if (page.Count == 0)
                 {
-                    allTokens.AddRange(page);
-                    onPageLoaded?.Invoke(page, pageIndex);
+                    if (pageIndex > 0)
+                        Debug.LogWarning($"[TezosExtensions] FetchAllPages — page {pageIndex + 1} " +
+                                         $"returned 0 results. Returning {allTokens.Count} token(s) fetched so far.");
+                    break;
                 }
+
+                allTokens.AddRange(page);
+                onPageLoaded?.Invoke(page, pageIndex);
 
                 // A page smaller than the requested limit means we have reached the end.
                 // This avoids an extra round-trip to confirm the inventory is exhausted.
                 if (page.Count < pageSize)
-                {
                     break;
-                }
 
                 pageIndex++;
             }
 
-            Debug.Log($"[TezosExtensions] Loaded {allTokens.Count} token(s) " +
-                      $"for {account} across {pageIndex + 1} page(s).");
+            Debug.Log($"[TezosExtensions] FetchAllPages — complete. " +
+                      $"{allTokens.Count} token(s) for {account} across {pageIndex + 1} page(s).");
 
             return allTokens;
+        }
+
+
+        // ---------------------------------------------------------------
+        //  HTTP helper — retry logic
+        // ---------------------------------------------------------------
+
+        /// <summary>
+        /// Sends an HTTP GET request to <paramref name="url"/>, retrying up to
+        /// <see cref="MaxRetries"/> times on transient failures before giving up.
+        ///
+        /// RETRY POLICY:
+        ///   Retried (temporary, worth trying again):
+        ///     - ConnectionError  — no network, DNS failure, timeout
+        ///     - HTTP 429         — Too Many Requests (TzKT rate limit)
+        ///     - HTTP 5xx         — TzKT server error
+        ///
+        ///   Not retried (permanent, retrying will not help):
+        ///     - HTTP 400         — malformed request URL (bug in this code)
+        ///     - HTTP 404         — resource does not exist
+        ///     - HTTP 4xx (other) — client error unlikely to resolve on retry
+        ///     - DataProcessingError — Unity failed to process the response body
+        ///
+        /// <paramref name="context"/> is a short description of the calling operation
+        /// used to prefix all log messages, making them easy to filter in the Console.
+        /// </summary>
+        private static async UniTask<HttpResult> SendWithRetryAsync(string url, string context)
+        {
+            for (int attempt = 1; attempt <= MaxRetries; attempt++)
+            {
+                try
+                {
+                    using (UnityWebRequest request = UnityWebRequest.Get(url))
+                    {
+                        await request.SendWebRequest();
+
+                        // SUCCESS
+                        if (request.result == UnityWebRequest.Result.Success)
+                        {
+                            if (attempt > 1)
+                                Debug.Log($"[TezosExtensions] {context} — succeeded on attempt {attempt}/{MaxRetries}.");
+                            return new HttpResult(true, request.downloadHandler.text, request.responseCode);
+                        }
+
+                        long statusCode = request.responseCode;
+                        string error    = request.error;
+                        bool isRetryable = IsRetryable(request.result, statusCode);
+
+                        // PERMANENT FAILURE — log full detail and return immediately
+                        if (!isRetryable || attempt == MaxRetries)
+                        {
+                            string detail = request.result == UnityWebRequest.Result.ProtocolError
+                                ? $"HTTP {statusCode}"
+                                : request.result.ToString();
+                            Debug.LogError(
+                                $"[TezosExtensions] {context} — failed after {attempt} attempt(s).\n" +
+                                $"  Result:  {detail}\n" +
+                                $"  Error:   {error}\n" +
+                                $"  URL:     {url}");
+                            return new HttpResult(false, null, statusCode);
+                        }
+
+                        // TRANSIENT FAILURE — warn and retry
+                        Debug.LogWarning(
+                            $"[TezosExtensions] {context} — attempt {attempt}/{MaxRetries} failed " +
+                            $"({request.result}, HTTP {statusCode}). " +
+                            $"Retrying in {RetryDelaySeconds}s...");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Catches unexpected exceptions from SendWebRequest or handler access.
+                    // Treat as a retryable failure unless we are out of attempts.
+                    if (attempt == MaxRetries)
+                    {
+                        Debug.LogError(
+                            $"[TezosExtensions] {context} — unhandled exception after {attempt} attempt(s).\n" +
+                            $"  Exception: {ex.GetType().Name}: {ex.Message}\n" +
+                            $"  URL:       {url}");
+                        return new HttpResult(false, null, 0);
+                    }
+
+                    Debug.LogWarning(
+                        $"[TezosExtensions] {context} — attempt {attempt}/{MaxRetries} threw " +
+                        $"{ex.GetType().Name}: {ex.Message}. Retrying in {RetryDelaySeconds}s...");
+                }
+
+                await UniTask.Delay(TimeSpan.FromSeconds(RetryDelaySeconds));
+            }
+
+            // Unreachable — the loop always returns or breaks before here.
+            return new HttpResult(false, null, 0);
+        }
+
+        /// <summary>
+        /// Returns true if the given result/status code combination is worth retrying.
+        /// Connection errors and server-side faults are transient; client errors are not.
+        /// </summary>
+        private static bool IsRetryable(UnityWebRequest.Result result, long statusCode)
+        {
+            if (result == UnityWebRequest.Result.ConnectionError)
+                return true;
+
+            if (result == UnityWebRequest.Result.ProtocolError)
+                return statusCode == 429   // Too Many Requests — back off and retry
+                    || statusCode >= 500;  // Server error — TzKT may be temporarily unavailable
+
+            // DataProcessingError and other results are not retryable.
+            return false;
+        }
+
+        /// <summary>
+        /// Truncates a string to <paramref name="maxLength"/> characters for safe log output.
+        /// Appends "…" when truncation occurs so it is clear the text is incomplete.
+        /// </summary>
+        private static string Truncate(string text, int maxLength)
+        {
+            if (string.IsNullOrEmpty(text) || text.Length <= maxLength)
+                return text ?? "(null)";
+            return text.Substring(0, maxLength) + "…";
+        }
+
+
+        // ---------------------------------------------------------------
+        //  HTTP result value type
+        // ---------------------------------------------------------------
+
+        /// <summary>
+        /// Carries the outcome of a single HTTP request out of <see cref="SendWithRetryAsync"/>.
+        /// Using a value type avoids a heap allocation per request compared to a class.
+        /// </summary>
+        private readonly struct HttpResult
+        {
+            /// <summary>True if the request completed with HTTP 2xx.</summary>
+            public readonly bool   Success;
+
+            /// <summary>Response body text. Null when Success is false.</summary>
+            public readonly string Text;
+
+            /// <summary>HTTP status code. 0 if no response was received (connection error).</summary>
+            public readonly long   StatusCode;
+
+            public HttpResult(bool success, string text, long statusCode)
+            {
+                Success    = success;
+                Text       = text;
+                StatusCode = statusCode;
+            }
         }
 
 
